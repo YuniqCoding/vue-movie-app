f4069420dcf389a2f570f20830578af35d08110a
YuniqCoding <rkdldk37@gmail.com>
2024-02-16 20:56:54 +0900

    Create project

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+export default{
+  namespaced: true,
+  state: () => ({
+    name: 'YURA',
+    email : 'totquf52@naver.com',
+    blog: 'https://blog.naver.com/totquf52',
+    phone: '+82-10-8653-7740',
+    image: 'https://product.cdn.cevaws.com/var/storage/images/_aliases/reference/media/feliway-2017/images/kor-kr/1_gnetb-7sfmbx49emluey4a/6341829-1-kor-KR/1_gNETb-7SfMBX49EMLUeY4A.jpg'
+  })
+}
